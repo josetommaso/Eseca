0272848570aaf15d3e92d224c41915d3a7e1f3bb
Jose Tommaso <tommaso_j@hotmail.com>
2019-06-03 19:44:28 +0100

    updating styles slider / logo slider added / favicon added

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,41 @@
-/* MAPBOX */
+$(document).ready(function(){
 
-mapboxgl.accessToken = 'pk.eyJ1IjoidG9tbWFzb2oiLCJhIjoiY2p2azVjb3JvMG02cTQ5bDI5M3QzMW5vdSJ9.pxBvyV5sNi-9D0O-MtNu8g';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-67.604268, 10.235346],
-    zoom: 15
+    /* MAPBOX */
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoidG9tbWFzb2oiLCJhIjoiY2p2azVjb3JvMG02cTQ5bDI5M3QzMW5vdSJ9.pxBvyV5sNi-9D0O-MtNu8g';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-67.604268, 10.235346],
+        zoom: 15
+    });
+
+    var marker = new mapboxgl.Marker()
+    .setLngLat([-67.604268, 10.235346])
+    .addTo(map);
+    
+    
+    /* SLICK SLIDER */
+
+    $('.logo-slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 4
+          }
+        }, {
+          breakpoint: 520,
+          settings: {
+            slidesToShow: 3
+          }
+        }]
+      });
+  
 });
-
-var marker = new mapboxgl.Marker()
-  .setLngLat([-67.604268, 10.235346])
-  .addTo(map);
